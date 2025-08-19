19751c8d2c15e46586e883c0ddc33809d8ba3f56
calsaa <136190324+calsaa@users.noreply.github.com>
2025-08-19 03:37:33 +0000

    1

--- app.py
+++ app.py
@@ -8,5 +8,34 @@ app = Flask(__name__)
 def index():
     return(render_template("index.html"))
 
+@app.route("/main",methods = ["GET","POST"])
+def main():
+    #database
+    return(render_template("main.html"))
+
+@app.route("/dbs",methods = ["GET","POST"])
+def dbs():
+    q = float(request.form.get("q"))
+    return(render_template("dbs.html", r = ((-50.6 * q) + 90.2)))
+
 if __name__ == "__main__":
     app.run()
+
+
+"""app.py
+from flask import Flask,render_template,request
+
+app = Flask(__name__)
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    return(render_template("index.html"))
+
+@app.route("/prediction",methods=["GET","POST"])
+def prediction():
+    q = float(request.form.get("q"))
+    return(render_template("prediction.html", r=(-50.6*q)+90.2))
+
+if __name__ == "__main__":
+    app.run()
+"""
